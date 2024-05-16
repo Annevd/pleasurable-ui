ce11e021d06a338488d689f5f140cae0ef227481
lisagjh <lisagjhoorn@outlook.com>
2024-05-16 13:50:03 +0200

    icon animation on click done

--- public/client.js
+++ public/client.js
@@ -1,11 +1,14 @@
 const menuBtn = document.querySelector(".menu-btn")
 const nav = document.querySelector("nav")
 const header = document.querySelector("header")
+const hamOne = document.querySelector(".rect-one")
+const hamTwo = document.querySelector(".rect-two")
+const hamThree = document.querySelector(".rect-three")
 
 menuBtn.addEventListener("click", function() {
     nav.classList.toggle("show")
-    header.style.backgroundColor = "#29292995"
-    header.style.width = "100vw"
-    header.style.height = "100vh"
+    hamOne.classList.toggle("cross-one")
+    hamTwo.classList.toggle("fade-rect")
+    hamThree.classList.toggle("cross-two")
 })
 
