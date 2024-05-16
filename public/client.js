6ae2aea68d9b880faed6f6c23e15c9264c0a1e3d
lisagjh <lisagjhoorn@outlook.com>
2024-05-16 15:20:23 +0200

    menu V2 done

--- public/client.js
+++ public/client.js
@@ -1,14 +1,21 @@
-const menuBtn = document.querySelector(".menu-btn")
-const nav = document.querySelector("nav")
-const header = document.querySelector("header")
-const hamOne = document.querySelector(".rect-one")
-const hamTwo = document.querySelector(".rect-two")
-const hamThree = document.querySelector(".rect-three")
+const menuBtn = document.querySelector(".menu-btn");
+const nav = document.querySelector("nav");
+const header = document.querySelector("header");
+const hamOne = document.querySelector(".rect-one");
+const hamTwo = document.querySelector(".rect-two");
+const hamThree = document.querySelector(".rect-three");
 
-menuBtn.addEventListener("click", function() {
-    nav.classList.toggle("show")
-    hamOne.classList.toggle("cross-one")
-    hamTwo.classList.toggle("fade-rect")
-    hamThree.classList.toggle("cross-two")
-})
+menuBtn.addEventListener("click", function () {
+  nav.classList.toggle("show");
+  menuBtn.classList.toggle("move-btn");
+  menuBtn.classList.toggle("cross")
+});
 
+// close menu when clicked outside of it
+document.onclick = function(e){
+    if (!menuBtn.contains(e.target) && !nav.contains(e.target) ) {
+        nav.classList.remove("show");
+        menuBtn.classList.remove("move-btn");
+        menuBtn.classList.remove("cross")
+    }
+}
